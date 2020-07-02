f5f3b0e008a90ab2ea9fefd0294dcea886001b59
kjh9267 <kjh9267z@naver.com>
2020-07-03 08:32:51 +0900

    Error Serializer for Response Body

--- src/test/java/me/jun/restapispring/events/EventControllerTest.java
+++ src/test/java/me/jun/restapispring/events/EventControllerTest.java
@@ -122,6 +122,11 @@ public class EventControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(eventDto)))
                 .andDo(print())
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].objectName").exists())
+                .andExpect(jsonPath("$[0].field").exists())
+                .andExpect(jsonPath("$[0].defaultMessage").exists())
+                .andExpect(jsonPath("$[0].code").exists())
+                .andExpect(jsonPath("$[0].rejectedValue").exists());
     }
 }
