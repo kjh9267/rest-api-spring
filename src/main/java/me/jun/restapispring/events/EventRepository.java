cdd227bad4a16ff3b7dbddea1cb72a412e5f9484
kjh9267 <kjh9267z@naver.com>
2020-07-03 06:27:44 +0900

    400 BadRequest for unknown properties.

--- src/main/java/me/jun/restapispring/events/EventRepository.java
+++ src/main/java/me/jun/restapispring/events/EventRepository.java
@@ -1,8 +1,6 @@
 package me.jun.restapispring.events;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
 }
--- src/test/java/me/jun/restapispring/events/EventControllerTest.java
+++ src/test/java/me/jun/restapispring/events/EventControllerTest.java
@@ -32,6 +32,36 @@ public class EventControllerTest {
 
     @Test
     public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("Rest API")
+                .beginEnrollmentDateTime(LocalDateTime.of(1970, 1, 1, 0, 0,0))
+                .closeEnrollmentDateTime(LocalDateTime.of(1970, 1, 2, 0, 0, 0))
+                .beginEventDateTime(LocalDateTime.of(1970, 1, 3, 0, 0, 0))
+                .endEventDateTime(LocalDateTime.of(1970, 1, 4, 0, 0, 0))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("seoul")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+
+    }
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -49,17 +79,11 @@ public class EventControllerTest {
                 .build();
 
         mockMvc.perform(post("/api/events/")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaTypes.HAL_JSON)
-                    .content(objectMapper.writeValueAsString(event)))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+                .andExpect(status().isBadRequest());
 
     }
 }
