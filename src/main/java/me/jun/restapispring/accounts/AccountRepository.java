ba804ee569afeddd4f380ade5dfa0408c2cacfd2
kjh9267 <kjh9267z@naver.com>
2020-07-06 18:21:54 +0900

    OAuth2: Load user by user name

---
+++ src/main/java/me/jun/restapispring/accounts/AccountRepository.java
@@ -0,0 +1,9 @@
+package me.jun.restapispring.accounts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Optional<Account> findByEmail(String userName);
+}
