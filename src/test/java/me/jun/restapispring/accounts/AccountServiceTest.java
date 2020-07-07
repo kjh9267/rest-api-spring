b2bba519a6543d83576644c52a81eacc994bcb80
kjh9267 <kjh9267z@naver.com>
2020-07-07 14:07:20 +0900

    Password Encoder Test

--- src/test/java/me/jun/restapispring/accounts/AccountServiceTest.java
+++ src/test/java/me/jun/restapispring/accounts/AccountServiceTest.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import sun.security.util.Password;
 
 import java.util.HashSet;
 
@@ -24,11 +26,11 @@ public class AccountServiceTest {
     @Autowired
     AccountService accountService;
 
-    @Autowired
-    AccountRepository accountRepository;
-
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Test
     public void findByUserName() {
@@ -37,21 +39,21 @@ public class AccountServiceTest {
         roles.add(AccountRole.ADMIN);
         roles.add(AccountRole.USER);
         String password = "pass";
-
         String userName = "user@email.com";
+
         Account account = Account.builder()
                 .email(userName)
                 .password(password)
                 .roles(roles)
                 .build();
 
-        accountRepository.save(account);
+        accountService.saveAccount(account);
 
         // When
         UserDetails userDetails = accountService.loadUserByUsername(userName);
 
         // Then
-        assertThat(userDetails.getPassword()).isEqualTo(password);
+        assertThat(passwordEncoder.matches(password, userDetails.getPassword())).isTrue();
     }
 
     @Test
